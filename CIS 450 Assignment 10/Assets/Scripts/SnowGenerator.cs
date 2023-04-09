a323218d5231a8930d20a96027a97630189d0e0d
Edgy-Floors <63993823+Edgy-Floors@users.noreply.github.com>
2023-04-09 14:02:41 -0500

    In progress

--- CIS 450 Assignment 10/Assets/Scripts/SnowGenerator.cs
+++ CIS 450 Assignment 10/Assets/Scripts/SnowGenerator.cs
@@ -4,14 +4,53 @@ using UnityEngine;
 
 public class SnowGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SnowGenerator instance = null;
+    public GameObject snowPrefab;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+    public SnowPool sp;
+
+    private SnowGenerator()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public static SnowGenerator getInstance()
+    {
+        if(instance == null)
+        {
+            instance = new SnowGenerator();
+        }
+        return instance;
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+
+
+    private void Start()
+    {
+        sp = GetComponent<SnowPool>();
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+        Queue<GameObject> objectPool = new Queue<GameObject>();
+        for(int i = 0; i < sp.poolSize; i++)
+        {
+            GameObject temp = Instantiate(sp.snowObject);
+            objectPool.Enqueue(temp);
+        }
+
+        poolDictionary.Add(sp.tag, objectPool);
+    }
+
+    private void Awake()
     {
         
     }
---
+++ CIS 450 Assignment 10/Assets/Scripts/SnowPool.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowPool : MonoBehaviour
+{
+    public int poolSize = 20;
+    public GameObject snowObject;
+    public string tag;
+}
